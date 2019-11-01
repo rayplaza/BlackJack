f086f929013986b819d6592c88465bd59e27269b
Ray Plaza <munditoplaza@gmail.com>
2019-11-01 14:41:11 -0700

    Updated JS with psuedocode

---
+++ js/app.js
@@ -0,0 +1,37 @@
+//display will first be clear with only buttons.
+
+// assign a value to each card in the "cards" array: Maybe I will have to have a key of Cards with each card having
+// a value for each card for later summing. 
+
+// sum function to calculate win loss or draw. (nacho libre audio quote plays depending on status)
+
+
+
+
+
+// ---------------------------------User Actions--------------------------------------------
+
+// User clicks on the "deal" button and a card is randomly drawn from the cards array.
+
+// First 2 cards will "randomly" be drawn from the "cards" array and placed in the  "playerHand" array and add
+// the sum of the "playerHand" array to the "playerScore" variable. Run sum function to see if player wins, loses or
+// draws with dealer.
+// (display both cards showing on the screen.)
+
+// Second another 2 cards will be "randomly" drawn from the "cards" array and placed in the "dealerHand" array and
+// add the sum of the "dealerHand" array to a hidden "dealerScore" variable. How will the dealer know when to hit or stay???
+// (display Front of card shown on one and back of card of the other.)
+
+
+
+
+// when "Hit" is clicked function starts to randomly take a card from the "cards" array and place in the "playerHand"
+// array. (display facing front) if the sum of the array is more then 21 then player loses, if it is less then 21 
+// then player can continue to hit or stand. 
+
+
+
+// when "stand" is clicked no action is taken and dealerHand runs.
+
+
+// Deal button resets the game at any point.
