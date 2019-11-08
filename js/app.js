caca1f08a244d582a090c45fb0f7c03b4e735547
Ray Plaza <munditoplaza@gmail.com>
2019-11-07 19:12:18 -0800

    code cleaned

--- js/app.js
+++ js/app.js
@@ -1,31 +1,5 @@
 
 
-// ---------------------------------functions--------------------------------------------//
-
-// PLAY function
-// First 2 cards will "randomly" be drawn from the "deck" array and placed in the  "playerHand" array and add
-// the sum of the "playerHand" array to the "playerScore" variable. Run sum function to see if player wins, loses or
-// draws with dealer. Run Dealer
-// (display both cards showing on the screen.)
-
-// (DEALER) another 2 cards will be "randomly" drawn from the "deck" array and placed in the "dealerHand" array and
-// add the sum of the "dealerHand" array to a hidden "dealerScore" variable. Run a dealer sum function. Wait for player
-// (display Front of card shown on one and back of card of the other.)
-
-// Player sum function to calculate win loss or draw. If player reaches 5 cards player wins(5 card Nacho) (nacho libre audio 
-// quote plays depending on status)
-
-// Dealer sum function to provide some AI. If dealer sum is less then 18 hit. If dealer sum is between 18 and 21 stay.
-
-//-----------------------------------Event Listeners--------------------------------------------------//
-
-// when "HIT" is clicked function starts to randomly take a card from the "deck" array and place in the "playerHand"
-// array. (display facing front) run sum function to see if win loss or draw. If all false then Dealer turn.
-
-// when "STAND" is clicked players game is done and dealerHand runs.
-
-// "DEAL" button resets the game at any point and starts play function
-
 
 // ---------------------------------------Cashed Element Ref-----------------------------------------------------//
 //                                      Storing the DOM element in a variable.
@@ -130,7 +104,6 @@ class Deck {
         dealerHand = [];
         while(dealerHand.length < 2) {
             dealerHand.push(this.deck.pop());
-            // dealerContainer.innerHTML = '<img id= card-back src="images/backs/red.svg">';
         }
         return dealerHand;
     }
@@ -357,11 +330,4 @@ function outcome() {
         }
     }
 }
-
-
-
-
-
-
-
 
